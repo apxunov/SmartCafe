0675e209e0f0c68c93feab3a324453165fed04a3
Максим Юхименко <moyukhimenko@edu.hse.ru>
2019-12-20 23:16:17 +0300

    Добавил два окна cashier

---
+++ SmartCafeApp.UI/CashierWindowLog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SmartCafeApp.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для CashierWindowLog.xaml
+    /// </summary>
+    public partial class CashierWindowLog : Window
+    {
+        public CashierWindowLog()
+        {
+            InitializeComponent();
+        }
+
+        private void Cashier_Log_Click(object sender, RoutedEventArgs e)
+        {
+            CashierModeWindow Win = new CashierModeWindow();
+            Win.Show();
+            this.Close();
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow Win = new MainWindow();
+            Win.Show();
+            this.Close();
+        }
+    }
+}

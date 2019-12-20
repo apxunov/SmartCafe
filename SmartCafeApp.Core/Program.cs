f55125b55e4d3f3a3113628e0378d1af2d488583
Сиражудин Салаватов <smsalavatov@edu.hse.ru>
2019-12-20 16:25:52 +0300

    мчмс

---
+++ SmartCafeApp.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartCafeApp.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
